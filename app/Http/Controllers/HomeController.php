1b3c9e42ac6c1f6d347de3abbdf47bfffcf64def
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-12 16:22:54 +0200

    added Movie model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
 
+        $movies = Movie::all();
+
         $data = [
-            "name" => "Pino"
+            "movie" => new Movie(),
+            "movies" => $movies,
         ];
 
         return view('movies.index', $data);
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -7,7 +7,9 @@
         <div class="row">
             <div class="col-12">
                 <h1>Elenco dei film:</h1>
-                {{ $name }}
+                <p>
+                    @dump($movies)
+                </p>
             </div>
         </div>
     </div>
