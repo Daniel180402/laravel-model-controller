2aa441e662865e01e003db9427fe2a9ae325898f
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-05-12 16:43:53 +0200

    movie cards generated via db

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,12 +5,22 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>Elenco dei film:</h1>
-                <p>
-                    @dump($movies)
-                </p>
+            <div class="col-12 ps-4">
+                <h1>List of movies:</h1>
             </div>
+            @foreach ($movies as $movie)
+            <div class="col-4">
+                <div class="card p-3 m-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Release date: {{ $movie->date }}</h6>
+                        <p class="card-text">Original title: {{ $movie->original_title }}</p>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 @endsection
